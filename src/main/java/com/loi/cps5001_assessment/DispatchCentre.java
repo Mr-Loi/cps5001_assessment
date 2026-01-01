b94bb137e7f9426cf003a139ea43dff84d3243d2
Your Name <kirthanloi@icloud.com>
2026-01-01 21:13:16 +0000

    added setter and getter methods to allocate units in the dispatch centre class

--- src/main/java/com/loi/cps5001_assessment/DispatchCentre.java
+++ src/main/java/com/loi/cps5001_assessment/DispatchCentre.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.loi.cps5001_assessment;
 
 /**
  *
  * @author kirth
  */
-public class DispatchCentre {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class DispatchCentre extends Node {
+
+    private List<ResponseUnit> units;
+
+    public DispatchCentre(String id, String name) {
+        super(id, name);
+        this.units = new ArrayList<>();
+    }
+
+    public void addUnit(ResponseUnit unit) {
+        units.add(unit);
+    }
+
+    public List<ResponseUnit> getAvailableUnits(ResourceType type) {
+        List<ResponseUnit> available = new ArrayList<>();
+        for (ResponseUnit unit : units) {
+            if (unit.isAvailable() && unit.getType() == type) {
+                available.add(unit);
+            }
+        }
+        return available;
+    }
 }
+

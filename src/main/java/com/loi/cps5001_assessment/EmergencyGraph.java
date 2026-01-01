0f425e291a407d1824ff28fd16ab7b1c53e47c12
Your Name <kirthanloi@icloud.com>
2026-01-01 22:04:53 +0000

    added adjacency list to emergency graph class

--- src/main/java/com/loi/cps5001_assessment/EmergencyGraph.java
+++ src/main/java/com/loi/cps5001_assessment/EmergencyGraph.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.loi.cps5001_assessment;
 
 /**
  *
  * @author kirth
  */
+import java.util.*;
+
 public class EmergencyGraph {
-    
+
+    private Map<Node, List<Edge>> adjacencyList = new HashMap<>();
+
+    public void addNode(Node node) {
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdge(Node from, Node to, double weight) {
+        adjacencyList.get(from).add(new Edge(to, weight));
+    }
+
+    public void removeEdge(Node from, Node to) {
+        adjacencyList.get(from)
+                     .removeIf(edge -> edge.getDestination().equals(to));
+    }
+
+    public void updateEdgeWeight(Node from, Node to, double newWeight) {
+        for (Edge edge : adjacencyList.get(from)) {
+            if (edge.getDestination().equals(to)) {
+                edge.setWeight(newWeight);
+            }
+        }
+    }
+
+    public List<Edge> getEdges(Node node) {
+        return adjacencyList.get(node);
+    }
 }
